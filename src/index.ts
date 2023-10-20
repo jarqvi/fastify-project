1578875cc4a78edffc56853849f6eddf06c2e0de
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-10-20 16:18:55 +0330

    feat: add config

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+
+import config from 'config';
+import { cleanEnv, str } from 'envalid';
+
+import IEnv from './interface/env';
+
+const APP_NAME: string = config.get('APP_NAME');
+const PORT: string = config.get('PORT');
+// ---
+const HOST: string = config.get('HOST');
+const DB_URI: string = config.get('DB_URI');
+const SECRET_KEY: string = config.get('SECRET_KEY');
+// ---
+const NODE_ENV = process.env.NODE_ENV;
+
+const env: IEnv = cleanEnv(process.env, {
+    NODE_ENV: str({ desc: 'development or production' }),
+    DB_URI: str({ desc: 'for postgres database' }),
+    HOST: str({ desc: 'the host of app' }),
+    SECRET_KEY: str({ desc: 'a secret for hashing' })
+}); 
+// ---
+env.NODE_ENV;
+env.DB_URI;
+env.HOST;
+env.SECRET_KEY;
---
+++ src/interface/env.ts
@@ -0,0 +1,6 @@
+export default interface IEnv {
+    NODE_ENV: string;
+    DB_URI: string;
+    HOST: string;
+    SECRET_KEY: string;
+}
