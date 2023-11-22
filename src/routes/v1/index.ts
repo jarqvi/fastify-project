2613616242a45126c1a6d0f434d54bec8b6a0f4c
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-11-22 22:01:11 +0330

    feat: dockerize project

---
+++ src/routes/v1/index.ts
@@ -0,0 +1,13 @@
+import { FastifyInstance, FastifyPluginOptions } from 'fastify';
+
+import authRoute from './auth';
+
+export default function v1Routes(
+    fastify: FastifyInstance,
+    options: FastifyPluginOptions,
+    done: (err?: Error | undefined) => void,
+) {
+    fastify.register(authRoute, { prefix: '/auth' });
+
+    done();
+}
--- src/startup/server.ts
+++ src/startup/server.ts
@@ -9,8 +9,8 @@ import { Sequelize } from 'sequelize';
 import { db } from './db';
 import { env } from './config';
 import { logger } from './logger';
-import IEnv from '../interfaces/env';
-import authRoute from '../routes/v1/auth';
+import v1Routes from '../routes/v1';
+import IRep from '../interfaces/rep';
 
 const PORT: number = config.get('PORT');
 const HOST: string = config.get('HOST');
@@ -36,7 +36,20 @@ server.register(cors, {
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 });
-server.register(authRoute, { prefix: '/api/v1' });
+
+server.route({
+    method: 'GET',
+    url: '/',
+    handler: (req, rep) => {
+      const rep200: IRep = {
+        statusCode: 200,
+        message: 'API web service started'
+      };
+
+      rep.status(200).send(rep200);
+    },
+})
+server.register(v1Routes, { prefix: '/api/v1' });
 
 server.listen({ port: +PORT, host: HOST }, async (err: Error | null, address: string) => {
   if (err) {
